44c938e8481f41c538cf5a9cfee233354ba84110
komisuomi <only-for-serzh@yandex.ru>
2020-10-19 22:57:40 +0300

    Added branch Manager1 + Lombok

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -1,44 +1,24 @@
 package ru.netology.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class Book extends Product {
     private String author;
     private int pages;
     private int publishedYear;
 
     public Book() {
-        super();
     }
 
     public Book(int id, String name, int price, String author, int pages, int publishedYear) {
         super(id, name, price);
         this.author = author;
         this.pages = pages;
-        this.publishedYear = publishedYear;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
-
-    public void setPublishedYear(int publishedYear) {
         this.publishedYear = publishedYear;
     }
 
--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -1,44 +1,19 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
     private int id;
     private String name;
     private int price;
 
-    public Product() {
-    }
-
-    public Product(int id, String name, int price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
 
     @Override
     public boolean equals(Object o) {
